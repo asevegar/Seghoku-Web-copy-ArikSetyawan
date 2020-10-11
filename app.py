f6b03f88f76bc8b68f17be650eb616fdaf6b9897
Arik Setyawan <setyawanarik@gmail.com>
2020-10-11 18:11:13 +0700

    Optimasi router /area/ dan /mitra/

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, jsonify, session, request
 from itsdangerous import URLSafeTimedSerializer, BadData, SignatureExpired
-import requests, random
+import requests, random, timeit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
@@ -25,12 +25,12 @@ def isnormal():
             return True
 
 # API URL
-url_menu = "https://seghoku-api.herokuapp.com/api/menu/"
-url_tenant = "https://seghoku-api.herokuapp.com/api/tenant/"
-url_location = "https://seghoku-api.herokuapp.com/api/locations/"
-url_auth = "https://seghoku-api.herokuapp.com/api/auth/"
-url_user = "https://seghoku-api.herokuapp.com/api/users/"
-url_level_user = "https://seghoku-api.herokuapp.com/api/level_user/"
+url_menu = "http://127.0.0.1:5000/api/menu/"
+url_tenant = "http://127.0.0.1:5000/api/tenant/"
+url_location = "http://127.0.0.1:5000/api/locations/"
+url_auth = "http://127.0.0.1:5000/api/auth/"
+url_user = "http://127.0.0.1:5000/api/users/"
+url_level_user = "http://127.0.0.1:5000/api/level_user/"
 
 
 
@@ -67,7 +67,6 @@ def index():
         data['tenant'] = tenant
 
         data_menu.append(data)
-        print(data_menu)
     # return jsonify(data_menu)
     return render_template("index.html", data_menu=data_menu)
 
@@ -75,101 +74,77 @@ def index():
 @app.route('/area/<place>')
 def area(place):
     try:
-        place = sToken.loads(place, salt="id_location", max_age=3600)
+        tempat = sToken.loads(place, salt="id_location", max_age=3600)
         data_menu = []
-        # Get location
-        req_location = requests.get(
-            url_location, params={'id_location': place})
-        req_location = req_location.json()
-        if req_location['status'] == 'error':
-            return redirect(url_for('index'))
-        else:
 
-            # data_location
-            req_location = req_location['data']
-            location = {
-                "id": sToken.dumps(req_location['id'], salt="id_location"),
-                "nama_location": req_location["nama_location"]
-            }
+        # request menu
+        req_menu = requests.get(url_menu, params={"id_location":tempat})
+        req_menu = req_menu.json()['data']
 
-            # get tenant by location
-            req_tenant = requests.get(
-                url_tenant, params={'id_location': req_location['id']})
-            req_tenant = req_tenant.json()['data']
-            for i in req_tenant:
-                tenant = {
-                    'id': sToken.dumps(i['id'], salt='id_tenant'),
-                    "nama_toko": i['nama_toko'],
-                    "no_hp": i['no_hp'],
-                    "location": location
-                }
-                # get menu by tenant
-                req_menu = requests.get(
-                    url_menu, params={"id_tenant": i['id']})
-                req_menu = req_menu.json()['data']
-                for j in req_menu:
-                    data = {}
-                    data['tenant'] = tenant
-                    data['id'] = sToken.dumps(j['id'], salt='id_menu'),
-                    data['nama_menu'] = j['nama_menu']
-                    data['harga_menu'] = j['harga_menu']
-                    data['foto_menu'] = "https://seghoku-api.herokuapp.com/api/files/?filename={}".format(
-                        j['foto_menu'])
-                    data_menu.append(data)
-            # return jsonify(data_menu)
-            return render_template("area.html", data_menu=data_menu, place=req_location['nama_location'])
+        # Shuffle data
+        req_menu = random.sample(req_menu,len(req_menu))
+        for i in req_menu:
+            data = {}
+            data['id'] = sToken.dumps(i['id'], salt="id_menu"),
+            data['nama_menu'] = i['nama_menu']
+            data['harga_menu'] = i['harga_menu']
+            data['foto_menu'] = i['foto_menu']
+
+            # get tenant
+            tenant = {}
+            tenant['id'] = sToken.dumps(i["tenant"]['id'], salt="id_tenant")
+            tenant['nama_toko'] = i["tenant"]['nama_toko']
+            tenant['no_hp'] = i["tenant"]['no_hp']
+
+            # get location
+            location = {}
+            location['id'] = sToken.dumps(i["tenant"]["location"]['id'], salt="id_location")
+            location['nama_location'] = i["tenant"]["location"]['nama_location']
+
+            # set place
+            place = i["tenant"]["location"]['nama_location']
+
+            tenant['location'] = location
+            data['tenant'] = tenant
+
+            data_menu.append(data)
+        return render_template("area.html", data_menu=data_menu, place=place)
     except BadData:
         return redirect(url_for('index'))
     except SignatureExpired:
         return redirect(url_for('index'))
 
-
 @app.route('/mitra/<id_tenant>')
 def mitra(id_tenant):
     try:
         id_tenant = sToken.loads(id_tenant, salt="id_tenant", max_age=3600)
+
         data_menu = []
 
         # get tenant
-        req_tenant = requests.get(url_tenant, params={'id_tenant': id_tenant})
+        req_tenant = requests.get(url_menu, params={'id_tenant': id_tenant})
         req_tenant = req_tenant.json()
         if req_tenant['status'] == "error":
             return redirect(url_for('index'))
         else:
             req_tenant = req_tenant['data']
             tenant = {
-                'id': sToken.dumps(req_tenant['id'], salt='id_tenant'),
-                "nama_toko": req_tenant['nama_toko'],
-                "no_hp": req_tenant['no_hp']
+                "id":sToken.dumps(req_tenant['id'],salt="id_tenant"),
+                "nama_toko":req_tenant['nama_toko'],
+                "no_hp": req_tenant['no_hp'],
+                "location":{
+                    "id":sToken.dumps(req_tenant['location']['id'], salt='id_location'),
+                    "nama_location":req_tenant['location']['nama_location']
+                }
             }
-
-            # get location
-            req_location = requests.get(
-                url_location, params={'id_location': req_tenant['id_location']})
-            req_location = req_location.json()['data']
-
-            location = {}
-            location['id'] = sToken.dumps(
-                req_location['id'], salt='id_location')
-            location['nama_location'] = req_location['nama_location']
-
-            tenant['location'] = location
-
-            # get menu by tenant
-            req_menu = requests.get(
-                url_menu, params={"id_tenant": req_tenant['id']})
-            req_menu = req_menu.json()['data']
-            for j in req_menu:
+            for j in req_tenant['menu']:
                 data = {}
                 data['tenant'] = tenant
                 data['id'] = sToken.dumps(j['id'], salt="id_menu"),
                 data['nama_menu'] = j['nama_menu']
                 data['harga_menu'] = j['harga_menu']
-                data['foto_menu'] = "https://seghoku-api.herokuapp.com/api/files/?filename={}".format(
-                    j['foto_menu'])
+                data['foto_menu'] = j['foto_menu']
                 data_menu.append(data)
-
-            # return jsonify(data_menu)
             return render_template("mitra.html", data_menu=data_menu, mitra=req_tenant['nama_toko'])
     except BadData:
         return redirect(url_for('index'))
@@ -199,8 +174,8 @@ def login():
                 res = req.json()
                 if res['status'] == 'success':
                     session['islogin'] = True,
-                    session['id_user'] = True,
-                    session['nama_user'] = res['data']['nama']
+                    session['id_user'] = res['data']['id'],
+                    session['nama_user'] = res['data']['nama'].capitalize()
 
                     id_level = res['data']['id_level']
 
@@ -209,10 +184,11 @@ def login():
 
                     if nama_level == 'Admin':
                         session['admin'] = True
+                        return redirect(url_for('dashboard'))
                     if nama_level == 'Regular':
                         session['regular'] = True
+                        return redirect(url_for('index'))
 
-                    return redirect(url_for('index'))
                 return redirect(url_user("login"))
             return redirect(url_for('login'))
 
@@ -285,6 +261,13 @@ def joinparter():
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route('/dashboard/menu')
+def dashboard_menu():
+    if isadmin():
+        return render_template('menu.html')
+    else:
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
