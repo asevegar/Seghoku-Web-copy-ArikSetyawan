de457f3680cd4d90fe3fe15b274e7262e88e6150
Arik Setyawan <setyawanarik@gmail.com>
2020-10-13 10:50:29 +0700

    Menambah Fitur Create,Update,dan Delete Menu

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, jsonify, session, request
+from flask import Flask, flash, render_template, redirect, url_for, jsonify, session, request
 from itsdangerous import URLSafeTimedSerializer, BadData, SignatureExpired
-import requests, random, timeit
+from PIL import Image
+import requests, random, timeit, io
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 sToken = URLSafeTimedSerializer('thisissecret')
 
 # Custom Function
-
-
 def islogin():
     if "islogin" in session:
         return True
@@ -23,6 +24,10 @@ def isnormal():
     if islogin():
         if 'regular' in session:
             return True
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # API URL
 url_menu = "http://127.0.0.1:5000/api/menu/"
@@ -47,7 +52,7 @@ def index():
 
     for i in req_menu:
         data = {}
-        data['id'] = sToken.dumps(i['id'], salt="id_menu"),
+        data['id'] = sToken.dumps(i['id'], salt="id_menu")
         data['nama_menu'] = i['nama_menu']
         data['harga_menu'] = i['harga_menu']
         data['foto_menu'] = i['foto_menu']
@@ -85,7 +90,7 @@ def area(place):
         req_menu = random.sample(req_menu,len(req_menu))
         for i in req_menu:
             data = {}
-            data['id'] = sToken.dumps(i['id'], salt="id_menu"),
+            data['id'] = sToken.dumps(i['id'], salt="id_menu")
             data['nama_menu'] = i['nama_menu']
             data['harga_menu'] = i['harga_menu']
             data['foto_menu'] = i['foto_menu']
@@ -121,31 +126,31 @@ def mitra(id_tenant):
 
         data_menu = []
 
-        # get tenant
-        req_tenant = requests.get(url_menu, params={'id_tenant': id_tenant})
-        req_tenant = req_tenant.json()
-        if req_tenant['status'] == "error":
+        # get menu
+        req_menu = requests.get(url_menu, params={'id_tenant': id_tenant})
+        req_menu = req_menu.json()
+        if req_menu['status'] == "error":
             return redirect(url_for('index'))
         else:
-            req_tenant = req_tenant['data']
+            req_menu = req_menu['data']
             tenant = {
-                "id":sToken.dumps(req_tenant['id'],salt="id_tenant"),
-                "nama_toko":req_tenant['nama_toko'],
-                "no_hp": req_tenant['no_hp'],
+                "id":sToken.dumps(req_menu['id'],salt="id_tenant"),
+                "nama_toko":req_menu['nama_toko'],
+                "no_hp": req_menu['no_hp'],
                 "location":{
-                    "id":sToken.dumps(req_tenant['location']['id'], salt='id_location'),
-                    "nama_location":req_tenant['location']['nama_location']
+                    "id":sToken.dumps(req_menu['location']['id'], salt='id_location'),
+                    "nama_location":req_menu['location']['nama_location']
                 }
             }
-            for j in req_tenant['menu']:
+            for j in req_menu['menu']:
                 data = {}
                 data['tenant'] = tenant
-                data['id'] = sToken.dumps(j['id'], salt="id_menu"),
+                data['id'] = sToken.dumps(j['id'], salt="id_menu")
                 data['nama_menu'] = j['nama_menu']
                 data['harga_menu'] = j['harga_menu']
                 data['foto_menu'] = j['foto_menu']
                 data_menu.append(data)
-            return render_template("mitra.html", data_menu=data_menu, mitra=req_tenant['nama_toko'])
+            return render_template("mitra.html", data_menu=data_menu, mitra=req_menu['nama_toko'])
     except BadData:
         return redirect(url_for('index'))
     except SignatureExpired:
@@ -184,6 +189,9 @@ def login():
 
                     if nama_level == 'Admin':
                         session['admin'] = True
+                        req_tenant = requests.get(url_tenant,params={"id_user":res['data']['id']})
+                        res_tenant = req_tenant.json()
+                        session['id_tenant']=res_tenant['data']['id']
                         return redirect(url_for('dashboard'))
                     if nama_level == 'Regular':
                         session['regular'] = True
@@ -260,12 +268,164 @@ def joinparter():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template("dashboard.html")
+    if isadmin():
+        return render_template("dashboard.html")
+    else:
+        return redirect(url_for('index'))
 
 @app.route('/dashboard/menu')
 def dashboard_menu():
     if isadmin():
-        return render_template('menu.html')
+
+        data_menu = []
+
+        # get menu
+        req_menu = requests.get(url_menu,{"id_tenant":session['id_tenant']})
+        req_menu = req_menu.json()
+        if req_menu['status'] == "error":
+            return redirect(url_for('index'))
+        else:
+            req_menu = req_menu['data']
+            tenant = {
+                "id":sToken.dumps(req_menu['id'],salt="id_tenant"),
+                "nama_toko":req_menu['nama_toko'],
+                "no_hp": req_menu['no_hp'],
+                "location":{
+                    "id":sToken.dumps(req_menu['location']['id'], salt='id_location'),
+                    "nama_location":req_menu['location']['nama_location']
+                }
+            }
+            for j in req_menu['menu']:
+                data = {}
+                data['tenant'] = tenant
+                data['id'] = sToken.dumps(j['id'], salt="id_menu")
+                data['nama_menu'] = j['nama_menu']
+                data['harga_menu'] = j['harga_menu']
+                data['foto_menu'] = j['foto_menu']
+                data_menu.append(data)
+            return render_template('menu.html',data_menu=data_menu)
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/create_menu', methods=['POST'])
+def create_menu():
+    if isadmin():
+        menu_name = request.form['menu_name']
+        menu_price = request.form['menu_price']
+        if 'menu_photo' not in request.files:
+            return "nope"
+        file = request.files['menu_photo']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return "nope"
+        if file and allowed_file(file.filename):
+            image = file
+            image.name = image.filename
+            image = io.BufferedReader(image)
+            data = {"id_tenant":session['id_tenant'],"nama_menu":menu_name,"harga_menu":menu_price}
+            req = requests.post(url_menu,data=data,files={"FotoMenu":image})
+            if req.status_code == 200:
+                res = req.json()
+                if res['status'] == 'success':
+                    flash("Created",'success')
+                    return redirect(url_for('dashboard_menu'))
+                else:
+                    flash("Opps Something Wrong!","error")
+                    return redirect(url_for('dashboard_menu'))
+            else:
+                flash("Opps Server Error!","error")
+                return redirect(url_for('dashboard_menu'))
+        else:
+            flash("File Not Allowed!","error")
+            return redirect(url_for('dashboard_menu'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/edit_menu/<id_menu>', methods=['POST'])
+def edit_menu(id_menu):
+    if isadmin():
+        id_menu = sToken.loads(id_menu,salt='id_menu',max_age=3600)
+        # get menu
+        req_menu = requests.get(url_menu,params={'id_menu':id_menu})
+        if req_menu.status_code == 200:
+            res = req_menu.json()
+            if res['status'] == 'success' and res['data']['id_tenant'] == session['id_tenant']:
+                # get data form
+                menu_name = request.form['menu_name']
+                menu_price = request.form['menu_price']
+                # pack data into dictionary fo requests
+                data = {"id_menu":id_menu,"nama_menu":menu_name,"harga_menu":menu_price}
+                # get form image data
+                file = request.files['menu_photo']
+                if file.filename == '':
+                    update_menu =  requests.put(url_menu,data=data)
+                    if update_menu.status_code == 200:
+                        res = update_menu.json()
+                        if res['status'] == 'success':
+                            flash("Updated","success")
+                            return redirect(url_for('dashboard_menu'))
+                        else:
+                            flash("Opps Something Wrong!","error")
+                            return redirect(url_for('dashboard_menu'))
+                    else:
+                        flash("Opps Server Error!","error")
+                        return "server error"
+                if file and allowed_file(file.filename):
+                    image = file
+                    image.name = image.filename
+                    image = io.BufferedReader(image)
+                    req = requests.put(url_menu,data=data,files={"FotoMenu":image})
+                    if req.status_code == 200:
+                        res = req.json()
+                        if res['status'] == 'success':
+                            flash("Updated","success")
+                            return redirect(url_for('dashboard_menu'))
+                        else:
+                            flash("Opps Something Wrong!","error")
+                            return redirect(url_for('dashboard_menu'))
+                    else:
+                        flash("Opps Server Error!","error")
+                        return redirect(url_for('dashboard_menu'))
+                else:
+                    flash("File Not Allowed!","error")
+                    return redirect(url_for('dashboard_menu'))
+            else:
+                flash("User Not Allowed!","error")
+                return redirect(url_for('dashboard_menu'))
+        else:
+            flash("Opps Server Error!","error")
+            return redirect(url_for('dashboard_menu'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/remove_menu/<id_menu>')
+def remove_menu(id_menu):
+    if isadmin():
+        id_menu = sToken.loads(id_menu,salt='id_menu',max_age=3600)
+        # get menu
+        req_menu = requests.get(url_menu,params={'id_menu':id_menu})
+        if req_menu.status_code == 200:
+            res = req_menu.json()
+            if res['status'] == 'success' and res['data']['id_tenant'] == session['id_tenant']:
+                req = requests.delete(url_menu,params={"id_menu":id_menu})
+                if req.status_code == 200:
+                    res = req.json()
+                    if res['status'] == 'success':
+                        flash("Deleted","success")
+                        return redirect(url_for('dashboard_menu'))
+                    else:
+                        flash("Opps Something Wrong!","error")
+                        return redirect(url_for('dashboard_menu'))
+                else:
+                    flash("Opps Server Error!","error")
+                    return redirect(url_for('dashboard_menu'))
+            else:
+                flash("User Not Allowed!")
+                return redirect(url_for('dashboard_menu'))
+        else:
+            flash("Opps Server Error","error")
+            return redirect(url_for('dashboard_menu'))
     else:
         return redirect(url_for('index'))
 
